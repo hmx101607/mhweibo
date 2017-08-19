c2c8eae8b32c78bbe3cdd1e746550811089bdb2c
Mason <hmx101607@gmail.com>
2017-08-19 09:35:49 +0800

    [add]完成登录不同情况的切换

--- weibo/weibo/AppDelegate.swift
+++ weibo/weibo/AppDelegate.swift
@@ -12,7 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var viewController : UIViewController {
+        let isLogin = WBAccountViewModel.shareIntance.isLogin
+        return isLogin ? WBWelcomeViewController() : WBTabbarViewController()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
@@ -20,37 +24,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UITabBar.appearance().tintColor = UIColor.orange
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = WBTabbarViewController()
+        window?.rootViewController = viewController
         window?.backgroundColor = UIColor.white
         window?.makeKeyAndVisible()
         return true
     }
     
-    /*func configShareSDK () {
-        ShareSDK.registerApp("b899c1e73f34", activePlatforms: [SSDKPlatformType.typeSinaWeibo.rawValue],
-                             onImport: {(platform : SSDKPlatformType) -> Void in
-                switch platform
-                {
-                case SSDKPlatformType.typeSinaWeibo:
-                    ShareSDKConnector.connectWeibo(WeiboSDK.classForCoder())
-                default:
-                    break
-                }
-        }, onConfiguration: {(platform : SSDKPlatformType , appInfo : NSMutableDictionary?) -> Void in
-            switch platform
-            {
-            case SSDKPlatformType.typeSinaWeibo:
-                //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
-                appInfo?.ssdkSetupSinaWeibo(byAppKey: weibo_appkey,
-                                            appSecret: weibo_appsecret,
-                                            redirectUri: weibo_redirect_uri,
-                                            authType: SSDKAuthTypeBoth)
-            default:
-                break
-            }
-        })
-    }*/
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
